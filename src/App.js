3f537f3077344e9656d592635bbf36b535fdd927
Abhinav Verma <109528157+abhinavvrm2020@users.noreply.github.com>
2023-07-12 10:02:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import Square from './grid/square';
 
+function App() {
+  return (
+    <div className="App">
+      <Square /> 
+    </div>
+  );
+}
+
+export default App;
